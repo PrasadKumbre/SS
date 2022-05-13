23865a51df67b8027a37195bd49cd29415614aa6
Prasad Kumbre <prasadkumbare@gmail.com>
2022-05-14 00:48:58 +0530

    Change 2.0

---
+++ Script.js
@@ -0,0 +1,17 @@
+function myFunction1() {
+  alert("Your Enquiry From Submitted Successfully!!");
+}
+
+function Button2() {
+  alert(
+    "Your Details has been Fetch Successfully we will try to contant you soon."
+  );
+}
+
+function Button3() {
+  alert("Thank You!! Your Feedback has been Recived Successfully.");
+}
+
+function Button4() {
+  alert("Thank You!! For Your Valuable Suggestions.");
+}
